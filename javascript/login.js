a7107f8d72231f47b7e0b94b826e656ccdddb1c6
Nakk47 <pozz.vathhd@gmail.com>
2024-12-11 21:34:44 +0700

    initial file

---
+++ javascript/login.js
@@ -0,0 +1,16 @@
+let eyeicon = document.getElementById("eye-icon");
+let passowrd = document.getElementById("password-input")
+
+eyeicon.onclick = function()
+{
+    if(passowrd.type == "password")
+    {
+        passowrd.type = "Text";
+        eyeicon.src = "../Assets/Login/eye-open.svg";
+    }
+    else
+    {
+        passowrd.type = "password";
+        eyeicon.src = "../Assets/Login/eye-closed.svg";
+    }
+}
